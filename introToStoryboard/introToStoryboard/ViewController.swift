345a21adbac18f0015e854ffa39190595f1bcebc
vionashah <101988056+vionashah@users.noreply.github.com>
2022-07-27 11:18:21 -0400

    Add files via upload
    first xcode project

---
+++ introToStoryboard/introToStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  introToStoryboard
+//
+//  Created by Scholar on 7/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    print ("this is a print statement - testing testing!")
+    }
+
+
+}
+
